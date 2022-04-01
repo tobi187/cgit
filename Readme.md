9f2f7ff9c993e59751df0ec4b06cccc579b9db66
tobi187 <fischerstobi1998@gmail.com>
2022-04-01 20:48:27 +0200

    fix

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Add gitignores to your project
+
+### Currently supported: csharp, fsharp, python, flutter
+
+## Linux
+Add following line to .bashrc > then source .bashrc
+alias cgit="/path/to/cgit/main.py"
+
+## Windows
--- main.py
+++ main.py
@@ -21,8 +21,11 @@ def main():
     parser = init_argparse()
     args = vars(parser.parse_args())
     
-    base_path = os.path(__file__)
-    file = os.path.join(base_path, "templates", args["l"])
-    destination = os.path.join(os.curdir(), ".gitignore")
+    base_path = os.path.abspath(os.path.dirname(__file__))
+    file = os.path.join(base_path, "templates", args["language"] + ".txt")
+    destination = os.path.join(os.getcwd(), ".gitignore")
 
     shutil.copy(file, destination)
+
+if __name__ == "__main__":
+    main()
